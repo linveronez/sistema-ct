b29e4500c1b84308ed1364c6eee75374a06d8cfe
linveronez <162514074+linveronez@users.noreply.github.com>
2026-02-11 18:54:32 -0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,5 @@
+const supabaseUrl = "https://ocrcqcqzvmdzvhlpykcd.supabase.co"
+const supabaseKey = "sb_publishable_2wiSDlRGeZKif9LDzRKj7Q_SJPl17t9"
+
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey)
+
