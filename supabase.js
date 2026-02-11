304fa20c17464306a40854671a2c4dd3b42e2d42
linveronez <162514074+linveronez@users.noreply.github.com>
2026-02-11 19:33:18 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,11 @@
-const supabaseUrl = "https://ocrcqcqzvmdzvhlpykcd.supabase.co"
-const supabaseKey = "sb_publishable_2wiSDlRGeZKif9LDzRKj7Q_SJPl17t9"
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 
-const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey)
+<script>
+  const SUPABASE_URL = "https://ocrcqcqzvmdzvhlpykcd.supabase.co";
+  const SUPABASE_ANON_KEY = "sb_publishable_2wiSDlRGeZKif9LDzRKj7Q_SJPl17t9";
 
+  const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );
+</script>
